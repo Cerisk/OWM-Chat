2dd43afdfb924ec50037b0e53ced8ba70084fb4e
Cerisk <waytoezy@gmail.com>
2022-05-29 23:01:45 -0500

    Updated files and added icon.png to root dir.

--- src/test/java/com/owmchat/OWMChatPluginTest.java
+++ src/test/java/com/owmchat/OWMChatPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.owmchat;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class OWMChatPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(OWMChatPlugin.class);
 		RuneLite.main(args);
 	}
 }

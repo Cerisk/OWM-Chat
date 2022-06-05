e88675f904878ebe852ceb68f63a853dc0d793f7
Cerisk <waytoezy@gmail.com>
2022-06-04 20:45:35 -0500

    Added files/code from Emoji and NPC Overhead Text Chat plugins to make it functional.

--- src/main/java/com/owmchat/OWMChatConfig.java
+++ src/main/java/com/owmchat/OWMChatConfig.java
@@ -8,12 +8,64 @@ import net.runelite.client.config.ConfigItem;
 public interface OWMChatConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+			keyName = "OWM Chat",
+			name = "Welcome to Old Wise Man chat",
+			description = "Login and put some specs on to see the chat old man! Code used from several plugins (ie Emoji, NPC Chat Overhead Dialogue)"
 	)
 	default String greeting()
 	{
-		return "Hello";
+		return "Replace here";
 	}
+
+	@ConfigItem(
+			keyName = "dialogBoxText",
+			name = "Display Dialog Box Text Overhead",
+			description = "Displays dialog in the dialog box above the corresponding NPC"
+	)
+	default boolean showDialogBoxText()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "ambientDialog",
+			name = "Display Ambient Dialog Overhead",
+			description = "Displays ambient dialog above NPCs"
+	)
+	default boolean showAmbientDialog()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "damageDialog",
+			name = "Display Damage Dialog Overhead",
+			description = "Displays damage dialog above NPCs"
+	)
+	default boolean showDamageDialog()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "deathDialog",
+			name = "Display Death Dialog Overhead",
+			description = "Displays death dialog above NPCs"
+	)
+	default boolean showDeathDialog()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "walkingDialog",
+			name = "Display Walking Dialog Overhead",
+			description = "Displays walking dialog above NPCs"
+	)
+	default boolean showWalkingDialog()
+	{
+		return false;
+	}
+	@ConfigItem(
+			keyName = "chatDialog",
+			name = "Display Overhead Dialog in Chat",
+			description = "Displays all enabled dialog in the chat"
+	)
+	default boolean enableChatDialog() { return false; }
 }
--- src/main/java/com/owmchat/OWMChatPlugin.java
+++ src/main/java/com/owmchat/OWMChatPlugin.java
@@ -1,48 +1,151 @@
 package com.owmchat;
 
+
 import com.google.inject.Provides;
-import javax.inject.Inject;
+import joptsimple.internal.Strings;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.*;
+import net.runelite.api.events.*;
+import net.runelite.api.events.OverheadTextChanged;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.chat.ChatColorType;
+import net.runelite.client.chat.ChatMessageBuilder;
+import net.runelite.client.chat.ChatMessageManager;
+import net.runelite.client.chat.QueuedMessage;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.NPCManager;
+//import net.runelite.client.plugins.OWMChat.dialog.DialogNpc;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.devtools.DevToolsButton;
+import net.runelite.client.util.Text;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 
 @Slf4j
 @PluginDescriptor(
-	name = "Old Wise Man Chat"
+		name = "Old Wise Man Chat"
 )
 public class OWMChatPlugin extends Plugin
 {
 	@Inject
 	private Client client;
+	private DevToolsButton lineOfSight;
 
 	@Inject
 	private OWMChatConfig config;
 
+	@Inject
+	private ChatMessageManager chatMessageManager;
+
+
+	@Inject
+	private ClientThread clientThread;
+
+	@Inject
+	private NPCManager npcManager;
+
+
+	private Actor actor = null;
+	private String lastNPCText = "";
+	private int actorTextTick = 0;
+	private String lastPlayerText = "";
+	private int playerTextTick = 0;
+
+	//private final Actor actor;
+
+	//private final String overheadText;
+
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		log.info("[OWM-Chat] Plugin Initialized Successfully");
+		sendChatMessage("[OWM-Chat] Plugin Initialized Successfully");
+
+
+	}
+
+	private ChatMessageType getMenuOption()
+	{
+		ChatMessage menuEntry = null;
+		return menuEntry.getType();
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info("Looks like that's enough, back to bed bud.");
+		sendChatMessage("[OWM-Chat] Looks like that's enough, back to bed bud.");
 	}
 
+/*
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void onGameStateChanged(GameStateChanged c)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		if (!c.getGameState().equals(GameState.LOADING))
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			return;
 		}
+
+		actor = null;
+		lastNPCText = "";
+		lastPlayerText = "";
+	}
+*/
+
+
+	/*@Subscribe
+	public void onAnimationChanged(AnimationChanged animationChanged)
+	{
+		final NPC npc = (NPC) animationChanged.getActor();
+		log.info("animation changed for: " + animationChanged.getActor().getName());
+		/*sendChatMessage("[OWM-Chat]: " + npc);
+		if (!(animationChanged.getActor() instanceof NPC))
+		{
+			sendChatMessage("[OWM-Chat]: " + npc);
+
+		}
+		if (animationChanged.getActor().getName() != null)
+		{
+
+			sendChatMessage("[OWM-Chat]: " + animationChanged.getActor().getName() + " says " + animationChanged.getActor().getOverheadText());
+
+		}
+
+	}
+*/
+	@Subscribe
+	public void onOverheadTextChanged(OverheadTextChanged event)
+	{
+
+		// log.info("animation changed for: " + otChanged.getActor().getName());
+		final String bot = event.getActor().getName();
+		final String message = event.getOverheadText();
+
+		sendChatMessage(bot + ": " + message);
+
+
+	}
+
+
+	private void sendChatMessage(String chatMessage)
+	{
+		final String message = new ChatMessageBuilder()
+				.append(ChatColorType.HIGHLIGHT)
+				.append(chatMessage)
+				.build();
+
+		chatMessageManager.queue(
+				QueuedMessage.builder()
+						.type(ChatMessageType.CONSOLE)
+						.runeLiteFormattedMessage(message)
+						.build());
 	}
 
 	@Provides
